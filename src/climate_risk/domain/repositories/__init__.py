126126276e5a159bcf1b9d72116aa07a79a9219d
Yan Cotta <yanpcotta@gmail.com>
2025-05-24 13:56:42 -0300

    create the init.py file for the repositories module to properly expose our repository interfaces

---
+++ src/climate_risk/domain/repositories/__init__.py
@@ -0,0 +1,14 @@
+"""Repository interfaces for domain entities."""
+from .base import (
+    Repository,
+    LocationRepository,
+    WeatherDataRepository,
+    RiskAssessmentRepository,
+)
+
+__all__ = [
+    'Repository',
+    'LocationRepository',
+    'WeatherDataRepository',
+    'RiskAssessmentRepository',
+]
